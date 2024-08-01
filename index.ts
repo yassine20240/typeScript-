36ca84cf2c21fc20a965610e96b91743e6f5fbe9
ali <ali@gmail.com>
2024-08-01 19:46:39 +0200

    save this changes

---
+++ index.ts
@@ -0,0 +1,127 @@
+
+/* how to decleare the type of data in type 
+const x: (boolean| number |boolean[]|string)[]=[3,4,true,false ,'string ', [true,false ]]
+
+
+
+ 
+// ?  this sempole means dosn't care the value of the variables 
+
+
+// let y= 3|2|4; how to give more than one value in type 
+
+
+
+ //there is how to give one 
+ //void: يُستخدم للدلالة على أن الدالة لا تُرجع قيمة.
+//never: يُستخدم للدلالة على أن الدالة لا تعود أبدًا إلى نقطة استدعائا ترمي استثناءً أو تحتوي على حلقة لا نهائية
+
+
+
+
+
+
+/*       enums 
+
+   Direction {
+    Up = 1,
+    Down = 2,
+    Left = 3,
+    Right = 4
+}
+
+*/
+
+
+
+/*
+let data = 'ali';
+console.log((data as string ).repeat(3))
+//you can add the type of element in typescript as html
+*/
+
+
+/* Here in this code we now how to use objct in type
+
+let obj :{
+    name : string,
+    id : number
+}={
+    name : 'lis',
+    id: 3 
+}
+
+id : 2 ; 
+name: 'sara';
+//
+
+interface user { 
+email : string ,
+num : number 
+}
+//interface is like give the value in objct
+let users : user={
+    email : 'ali ',
+    num : 3 
+}
+    */
+
+
+
+
+
+
+
+
+/*
+interface pEroson {
+    age : number ,
+    name : string 
+}
+
+
+const  user:pEroson = {
+    name : 'ali',
+    age : 34,
+}
+
+
+*/
+
+ 
+
+/**
+ * 
+readonly :you can't change him 
+private
+public 
+
+}
+ */
+
+
+/*class pizza extends food {
+    //هذه هي الوراثة  extends 
+}
+*/
+
+
+
+/*
+this is genarel type in typescript 
+
+function hello  <T> (age:T){
+    console.log(age )
+}
+hello(34)
+ 
+
+*/
+
+
+
+
+
+
+
+
